d1bf85271f1461f3dcd634ee23483ebcf67d23fe
Melvyn Malherbe <melvyn.malherbe@qoqa.com>
2020-04-12 19:34:59 +0200

    restructuration items's list with jquery, and some item style

--- app/static/js/category.js
+++ app/static/js/category.js
@@ -83,68 +83,76 @@ function editCategory(idCategory) {
 }
 
 // items - edit (id for know what input need to be changed)
-function toggleEditItem(id) {
-  let name = document.getElementById("item-name-" + id);
-  let description = document.getElementById("item-description-" + id);
-  let button = document.getElementById("item-button-" + id);
+function onEditItem(id) {
+  $("#item-name-" + id)
+    .prop("disabled", false)
+    .focus();
+  $("#item-description-" + id).prop("disabled", false);
+  $(`#item-edit-${id}`).hide();
+  $(`#item-save-${id}`).show();
+}
 
-  if (name.disabled) {
-    name.disabled = false;
-    name.focus();
-    description.disabled = false;
-    button.innerHTML = "Save";
-    button.classList.remove("btn-link");
-    button.classList.add("btn-warning");
-  } else {
-    name.disabled = true;
-    description.disabled = true;
-    button.innerHTML = "Edit";
-    button.classList.add("btn-link");
-    button.classList.remove("btn-warning");
-    editItem(name.value, description.value, id, (state) =>
-      toggleSnackBar(state.status, state.state)
-    );
+function onSaveItem(id) {
+  const name = $("#item-name-" + id);
+  name.prop("disabled", true);
+  const description = $("#item-description-" + id);
+  description.prop("disabled", true);
+
+  if (name.val().length <= 2 || name.val().length >= 50) {
+    toggleSnackBar("Name need to have between 2 and 50 caracters.", "danger");
+    return;
   }
+  if (description.val().length < 5 || description.val().length >= 1000) {
+    toggleSnackBar(
+      "Description need to have between 5 and 1000 caracters.",
+      "danger"
+    );
+    return;
+  }
+
+  $(`#item-edit-${id}`).show();
+  $(`#item-save-${id}`).hide();
+  updateItem(id);
 }
 
 // function to edit an item, API
-function editItem(name, description, idItem, callback) {
+function updateItem(idItem) {
+  const name = $("#item-name-" + idItem).val();
+  const description = $("#item-description-" + idItem).val();
+
   const newItem = { item: { name: name, description: description } };
   const newItemJson = JSON.stringify(newItem);
 
-  fetch(`${API_URL}/items/${idItem}`, {
+  $.ajax({
+    url: `${API_URL}/items/${idItem}`,
     method: "PATCH",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+    data: newItemJson,
+    dataType: "json",
+    contentType: "application/json",
+    Accept: "application/json",
+    success: (state) => {
+      toggleSnackBar(state.status, state.state);
     },
-    body: newItemJson,
-  }).then((reponse) => {
-    if (reponse.status !== 200) {
-      console.warn("Tiqeto api has problem.");
-      callback && callback({ state: "danger", status: "error server" });
-      return;
-    }
-
-    reponse.json().then((state) => {
-      callback && callback(state);
-    });
+    error: (result) => {
+      console.warn("Request status :", result.status);
+      toggleSnackBar("Database has problem. Try an other time.", "danger");
+    },
   });
 }
 
 function displayCreateBox() {
-  document.getElementById("item-new-box").classList.add("hide");
+  $("#box-hide-item").hide();
 }
 
 // for categories.html
 // function to try if champs is correct on create new category
 function onSubmitCategory() {
-  let title = document.getElementById("name-category-new").value;
+  let title = $("#name-category-new").val();
 
   if (title.length < 2 || title.length > 50) {
     toggleSnackBar("Title need to have between 3 and 49 caractes.", "danger");
     return;
   }
 
-  document.getElementById("form-categories").submit();
+  $("#form-categories").submit();
 }
