b39d96f1e1ec7a86aec41118906074e252e327fa
Melvyn Malherbe <melvyn.malherbe@qoqa.com>
2020-04-13 19:26:02 +0200

    restructuration tiqet title jquery

--- app/static/js/tiqet.js
+++ app/static/js/tiqet.js
@@ -1,40 +1,26 @@
 /* title script "title.html" */
 function toggleEditTitle() {
-  const title = document.getElementById("title");
-  const img = document.getElementById("image-title");
-  const input = document.getElementById("input-title");
-  const button = document.getElementById("button-title");
-
-  title.classList.add("d-none");
-  img.classList.add("d-none");
-  input.classList.remove("d-none");
-  input.value = title.innerHTML;
-  input.addEventListener("keyup", titleSave);
-  input.focus();
-  button.classList.remove("d-none");
+  $("#title").hide();
+  $("#image-title").hide();
+  $("#input-title").show().val($("#title").val()).on("keyup", titleSave).focus();
+  $("#button-title").show();
 }
 
 function titleSave(event) {
   if (event) if (event.key !== "Enter") return;
+  const input = $("#input-title");
 
-  const title = document.getElementById("title");
-  const img = document.getElementById("image-title");
-  const input = document.getElementById("input-title");
-  const button = document.getElementById("button-title");
-
-  if (input.value.length < 3 || input.value.length > 50) {
+  if (input.val().length < 3 || input.val().length > 50) {
     toggleSnackbar("Title need to have between 4 and 49 caractes.", "danger");
-    return;
+  } else {
+    $("#image-title").show();
+    input.hide();
+    $("#title").html(input.val()).show();
+    $("#button-title").hide();
+    editTiqet(TIQET_ID, { title: input.val() }, (state) => {
+      toggleSnackbar(state.status, state.state);
+    });
   }
-
-  title.classList.remove("d-none");
-  img.classList.remove("d-none");
-  input.classList.add("d-none");
-  title.innerHTML = input.value;
-  button.classList.add("d-none");
-  editTiqet(TIQET_ID, { title: input.value }, (state) => {
-    toggleSnackbar(state.status, state.state);
-  });
 }
 
 /* content script "content.html" */
