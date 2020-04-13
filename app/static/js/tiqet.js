d776c36b085d5eff84bb01e057e5c62e3fbb4ba6
Melvyn Malherbe <melvyn.malherbe@qoqa.com>
2020-04-13 19:35:14 +0200

    retruct. tiqet content - jquery
    /!\ beug /!\ line break in text-field

--- app/static/js/tiqet.js
+++ app/static/js/tiqet.js
@@ -25,39 +25,30 @@ function titleSave(event) {
 
 /* content script "content.html" */
 function toggleEditContent() {
-  const content = document.getElementById("content");
-  const img = document.getElementById("image-content");
-  const input = document.getElementById("input-content");
-  const button = document.getElementById("button-content");
-
-  content.classList.add("d-none");
-  img.classList.add("d-none");
-  input.classList.remove("d-none");
-  input.value = content.innerHTML;
-  input.addEventListener("keyup", contentSave);
-  input.focus();
-  button.classList.remove("d-none");
+  $("#content").hide();
+  $("#image-content").hide();
+  $("#input-content")
+    .show()
+    .val($("#content").text())
+    .on("keyup", contentSave)
+    .focus();
+  $("#button-content").show();
 }
 
 function contentSave(event) {
   if (event) if (event.key !== "Enter") return;
 
-  const content = document.getElementById("content");
-  const img = document.getElementById("image-content");
-  const input = document.getElementById("input-content");
-  const button = document.getElementById("button-content");
+  const input = $("#input-content");
 
-  if (input.value.length < 2 || input.value.length > 5000) {
+  if (input.val().length < 2 || input.val().length > 5000) {
     toggleSnackbar("content need to have between 43and 4999 caractes.", "danger");
     return;
   }
-
-  content.classList.remove("d-none");
-  img.classList.remove("d-none");
-  input.classList.add("d-none");
-  content.innerHTML = input.value;
-  button.classList.add("d-none");
-  editTiqet(TIQET_ID, { content: input.value }, (state) => {
+  input.hide();
+  $("#content").show().text($("#input-content").val());
+  $("#image-content").show();
+  $("#button-content").hide();
+  editTiqet(TIQET_ID, { content: input.val() }, (state) => {
     toggleSnackbar(state.status, state.state);
   });
 }
